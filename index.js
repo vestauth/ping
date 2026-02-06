4b19ef348ed5cb3b807902d3ad1ea65dff679464
Scott Motte <mot@mot.la>
2026-02-06 12:41:59 -0800

    add test endpoint

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const vestauth = require('vestauth')
 
 const app = express()
 app.use(express.json())
-app.use(express.static('public'))
 
 function randomLatLng () {
   const u = Math.random()
@@ -41,6 +40,10 @@ function getGeo (req) {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/test', (req, res) => {
+  res.json({ ok: true })
 })
 
 app.post('/', async (req, res) => {
